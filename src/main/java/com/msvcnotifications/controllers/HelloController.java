a39ed69fae03966ffd21a320b34ec0d8f6019295
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 12:24:30 -0500

    configuration

---
+++ src/main/java/com/msvcnotifications/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.msvcnotifications.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String saludo() {
+        return "Hola Microservicio Notifications";
+    }
+}
