5affe1a52f324661f5ee4e0de49987ac7280ba6e
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 19:50:14 -0500

    configuration

--- src/main/java/com/msvcnotifications/MsvcNotificationsApplication.java
+++ src/main/java/com/msvcnotifications/MsvcNotificationsApplication.java
@@ -2,8 +2,9 @@ package com.msvcnotifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MsvcNotificationsApplication {
 
     public static void main(String[] args) {
